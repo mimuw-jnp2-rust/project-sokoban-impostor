1f2b3012725755aedd5a2659ee5365d147efe4b7
Jacus <mr.mug2901@gmail.com>
2023-01-06 16:42:43 +0100

    slight refactor in handle_ice + level

--- src/game/movement/animation.rs
+++ src/game/movement/animation.rs
@@ -80,59 +80,60 @@ pub fn handle_ice(
     board: Res<Board>,
     mut writer: EventWriter<MoveEvent>,
 ) {
-    if timer.0.finished() {
-        let mut positions_on_ice = Vec::new();
-        let direction = movement_data
-            .direction
-            .expect("No direction after animation");
-        for position in movement_data.moved_positions.iter() {
-            let position = *position;
-            if board.get_floor_type(position) == Floor::Ice {
-                let object = board.get_object_type(position.next_position(direction));
-                match object {
-                    GameObject::Empty => positions_on_ice.push(position),
-                    GameObject::Box => {
-                        if movement_data.moved_positions.contains(&position.next_position(direction)) {     //found box is already moving
-                            positions_on_ice.push(position);
-                        }
-                        else if board.get_floor_type(position.next_position(direction)) == Floor::Ice {
-                            let mut last_box_position = position.next_position(direction);
-                            let mut next_object_position = last_box_position.next_position(direction);
-                            let mut next_object = board.get_object_type(next_object_position);
-                            while next_object == GameObject::Box && board.get_floor_type(next_object_position) == Floor::Ice {
-                                last_box_position = next_object_position;
-                                next_object_position = next_object_position.next_position(direction);
-                                next_object = board.get_object_type(next_object_position);
-                            }
-                            if next_object == GameObject::Empty {
-                                positions_on_ice.push(last_box_position);
-                            }
-                            break;
-                            //either way the entity that encountered a stationary entity in front of it must stop, and so do entities before it
-                        }
-                        else {
-                            break;
-                        }
+    if !timer.0.finished() {
+        return;
+    }
+    let mut positions_on_ice = Vec::new();
+    let direction = movement_data
+        .direction
+        .expect("No direction after animation");
+    for position in movement_data.moved_positions.iter() {
+        let position = *position;
+        if board.get_floor_type(position) != Floor::Ice {
+            break;      //break in this loop means that this object and all that come before it stop movement
+        }
+        let object = board.get_object_type(position.next_position(direction));
+        match object {
+            GameObject::Empty => positions_on_ice.push(position),
+            GameObject::Box => {
+                if movement_data.moved_positions.contains(&position.next_position(direction)) {     //found box is already moving
+                    positions_on_ice.push(position);
+                }
+                else if board.get_floor_type(position.next_position(direction)) == Floor::Ice {
+                    // if there are multiple stationary boxes ahead, either the last one moves
+                    // (if it's on ice) or they remain stationary otherwise
+                    let mut last_box_position = position.next_position(direction);
+                    let mut next_object_position = last_box_position.next_position(direction);
+                    let mut next_object = board.get_object_type(next_object_position);
+                    while next_object == GameObject::Box && board.get_floor_type(next_object_position) == Floor::Ice {
+                        last_box_position = next_object_position;
+                        next_object_position = next_object_position.next_position(direction);
+                        next_object = board.get_object_type(next_object_position);
                     }
-                    _ => break,
+                    if next_object == GameObject::Empty {
+                        positions_on_ice.push(last_box_position);
+                    }
+                    break;
+                    //either way the entity that encountered a stationary entity in front of it must stop, and so do entities before it
+                }
+                else {
+                    break;
                 }
             }
-            else {
-                break;
-            }
+            _ => break,
         }
-        if positions_on_ice.len() == 0 {
-            app_state
-                .push(GameState::Static)
-                .expect("Could not correctly finish movement animation");
-        } else {
-            movement_data.direction = None;
-            movement_data.moved_positions.clear();
-            writer.send(MoveEvent {
-                direction,
-                positions: positions_on_ice,
-            });
-            timer.0.reset();
-        }
+    }
+    if positions_on_ice.is_empty() {
+        app_state
+            .push(GameState::Static)
+            .expect("Could not correctly finish movement animation");
+    } else {
+        movement_data.direction = None;
+        movement_data.moved_positions.clear();
+        writer.send(MoveEvent {
+            direction,
+            positions: positions_on_ice,
+        });
+        timer.0.reset();
     }
 }
